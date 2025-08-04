9f415a4864a29ae9dd29bc44246a410bcfe82dc9
Kafi <ahkafi05@gmail.com>
2025-08-05 02:30:22 +0600

    profile icon added in dashboard

--- src/Components/Dashboard/Dashboard.jsx
+++ src/Components/Dashboard/Dashboard.jsx
@@ -1,12 +1,26 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import useRole from '../Hooks/useRole';
 import { Link, Outlet, NavLink } from 'react-router';
 import logo from '../../assets/logo.png';
-import { HeartHandshake, LayoutDashboard, Users, FilePen, User, Home } from 'lucide-react';
+import { HeartHandshake, LayoutDashboard, Users, FilePen, User, Home, LogOut } from 'lucide-react';
 import Loading from '../Utils/Loading';
+import AuthContext from '../Auth/AuthContext';
+import toast from 'react-hot-toast';
 
 const Dashboard = () => {
     const {role, loading} = useRole()
+    const {user, signOutUser, setUser} = useContext(AuthContext)
+
+    const handleSignOut=()=>{
+        signOutUser()
+        .then(()=>{
+            toast.success('LogOut Successfully!')
+            setUser(null)
+        })
+        .catch((error)=>{
+            toast.error(error.code)
+        })
+    }
 
     if(loading) return <Loading></Loading>
   
@@ -43,33 +57,57 @@ const Dashboard = () => {
         {/* Overlay on small devices */}
         <label htmlFor="dashboard-drawer" className="drawer-overlay"></label>
 
-        <ul className="menu p-6 w-80 min-h-full bg-base-100 text-base-content lg:text-md font-medium space-y-2">
-          <li><NavLink to="/dashboard" end className={({isActive})=>`${isActive && 'bg-black/85 text-white'} py-2`}><LayoutDashboard size={16} />Dashboard</NavLink></li>
-        {
-            role === "admin" && 
-            <>
-                <li><NavLink to="/dashboard/all-users" className={({isActive})=>`${isActive && 'bg-black/85 text-white'} py-2`}><Users size={16} />All Users</NavLink></li>
-                <li><NavLink to="/dashboard/all-blood-donation-request" className={({isActive})=>`${isActive && 'bg-black/85 text-white'} py-2`}><HeartHandshake size={16} />Blood Donation Request</NavLink></li>
-                <li><NavLink to="/dashboard/content-management" className={({isActive})=>`${isActive && 'bg-black/85 text-white'} py-2`}><FilePen size={16} />Content Management</NavLink></li>
-            </>
-        }
-        {
-            role === "donor" && 
-            <>
-                <li><NavLink to="/dashboard/my-donation-requests" className={({isActive})=>`${isActive && 'bg-black/85 text-white'} py-2`}><Users size={16} />My Donation Request</NavLink></li>
-                <li><NavLink to="/dashboard/create-donation-request" className={({isActive})=>`${isActive && 'bg-black/85 text-white'} py-2`}><Users size={16} />Create Donation Request</NavLink></li>
-            </>
-        }
-        {
-            role === "volunteer" && 
-            <>
-                <li><NavLink to="/dashboard/all-blood-donation-request" className={({isActive})=>`${isActive && 'bg-black/85 text-white'} py-2`}><HeartHandshake size={16} />Blood Donation Request</NavLink></li>
-                <li><NavLink to="/dashboard/content-management" className={({isActive})=>`${isActive && 'bg-black/85 text-white'} py-2`}><FilePen size={16} />Content Management</NavLink></li>
-            </>
-        }
-        <li><NavLink to="/dashboard/profile" className={({isActive})=>`${isActive && 'bg-black/85 text-white'} py-2`}><User size={16} />Profile</NavLink></li>
-        <li><Link to="/" className="py-2"><Home size={16} />Home</Link></li>
-        </ul>
+        <div className='flex flex-col justify-between h-full w-80 bg-base-100'>
+              <ul className="menu p-6 flex-1 text-base-content lg:text-md font-medium space-y-2">
+                <li><NavLink to="/dashboard" end className={({isActive})=>`${isActive && 'bg-black/85 text-white'} py-2`}><LayoutDashboard size={16} />Dashboard</NavLink></li>
+              {
+                  role === "admin" && 
+                  <>
+                      <li><NavLink to="/dashboard/all-users" className={({isActive})=>`${isActive && 'bg-black/85 text-white'} py-2`}><Users size={16} />All Users</NavLink></li>
+                      <li><NavLink to="/dashboard/all-blood-donation-request" className={({isActive})=>`${isActive && 'bg-black/85 text-white'} py-2`}><HeartHandshake size={16} />Blood Donation Request</NavLink></li>
+                      <li><NavLink to="/dashboard/content-management" className={({isActive})=>`${isActive && 'bg-black/85 text-white'} py-2`}><FilePen size={16} />Content Management</NavLink></li>
+                  </>
+              }
+              {
+                  role === "donor" && 
+                  <>
+                      <li><NavLink to="/dashboard/my-donation-requests" className={({isActive})=>`${isActive && 'bg-black/85 text-white'} py-2`}><Users size={16} />My Donation Request</NavLink></li>
+                      <li><NavLink to="/dashboard/create-donation-request" className={({isActive})=>`${isActive && 'bg-black/85 text-white'} py-2`}><Users size={16} />Create Donation Request</NavLink></li>
+                  </>
+              }
+              {
+                  role === "volunteer" && 
+                  <>
+                      <li><NavLink to="/dashboard/all-blood-donation-request" className={({isActive})=>`${isActive && 'bg-black/85 text-white'} py-2`}><HeartHandshake size={16} />Blood Donation Request</NavLink></li>
+                      <li><NavLink to="/dashboard/content-management" className={({isActive})=>`${isActive && 'bg-black/85 text-white'} py-2`}><FilePen size={16} />Content Management</NavLink></li>
+                  </>
+              }
+            </ul>
+            {/* profile section */}
+            <div className='p-6 flex items-center space-x-2'>
+               <div className="dropdown dropdown-right dropdown-top">
+               <div tabIndex={0} role="button" className="m-1">
+                   <div className="avatar">
+                       <div className="ring-gray-900  ring-offset-base-100 w-8 rounded-full ring-2 ring-offset-2">
+                           <img src={user.photoURL} />
+                       </div>
+                   </div>
+               </div>
+               <ul tabIndex={0} className="dropdown-content menu bg-base-100 rounded-box z-1 w-auto p-2 shadow-sm">
+                   <li>
+                       <Link to='/'>
+                           <Link className='flex items-center'><Home size={16} className='mr-2'></Home> Home</Link>
+                       </Link>
+                   </li>
+                    <li><NavLink to="/dashboard/profile" className={({isActive})=>`${isActive && 'bg-black/85 text-white'} py-2`}><User size={16} />Profile</NavLink></li>
+                   <li>
+                       <button onClick={handleSignOut}><LogOut size={16}/> LogOut</button>
+                   </li>
+               </ul>
+               </div>
+               <p className='font-normal'>{user.displayName}</p>
+            </div>
+        </div>
       </div>
     </div>
     );
